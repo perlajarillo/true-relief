e449114ba51d7e03d43d4051c8b488f1678f2a47
perlajarillo <perlai.jarillo@gmail.com>
2018-07-03 10:16:10 -0400

    The states are being changed according to the firebase.Promise return. Also, changes in the view are included now in the render(). Validation of the password has been included.

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -9,13 +9,9 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import firebase from '../firebase.js';
-import Header from '../Header/Header';
 // import route Components
 import {
-  BrowserRouter as Router,
-  Route,
   Link,
-  Switch,
   Redirect
 } from 'react-router-dom'
 const styles = theme => ({
@@ -23,17 +19,11 @@ const styles = theme => ({
     margin: theme.spacing.unit * 6,
 
   },
-  /* container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-  }, */
   formControl: {
     margin: theme.spacing.unit,
   },
   button: {
     margin: theme.spacing.unit,
-    /* fullWidth: 'false', */
   },
   textField: {
     maxWidth: 400,
@@ -54,83 +44,67 @@ class SignUp extends React.Component {
       password1: '',
       password2: '',
       error: '',
-      redirectToRegister:false
+      redirectToRegister: false
 
     };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    console.log('target', target, 'name', name, 'value', value);
+    const { target } = event;
+    const { value, name } = target;
     this.setState({
-      [name]: value,
-      [name]: value,
-      [name]: value,
-      [name]: value,
       [name]: value
-
     });
   };
 
   handleSubmit(e) {
     e.preventDefault();
-    var email=this.state.mail;
-    var password=this.state.password1;
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-       console.log(error.code);
-       console.log(error.message);
-    });
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-       console.log(error.code);
-       console.log(error.message);
-    });
-    //If there is a currentUser means the user press logOut in an active session.
-    const hidenMenus=document.getElementById('hidenMenus');
-    const defaultMenus=document.getElementById('defaultMenus');
-    var user = firebase.auth().currentUser;
-    //If the user was autenthicated then we show the propor menus
-    firebase.auth().onAuthStateChanged(user =>{
-      if (user) {
-      alert("User made log in: "+user.email+ " and we must redirect to the demographic section");
-      hidenMenus.classList.remove('menuHide');
-      defaultMenus.classList.add('menuHide');
-      this.setState({
-      mail: '',
-      password1: '',
-      password2: '',
-      error:'',
-      redirectToRegister:true
-    });
-    } 
-    else
-    {
-      this.setState({
-      mail: '',
-      password1: '',
-      password2: '',
-      error:'The account was not created!',
-      redirectToRegister:false
-    });
+    var email = this.state.mail;
+    var password = this.state.password1;
+    var password2 = this.state.password2;
+    //password and password confirmation must be equal
+    if (password == password2) {
+      //Trying to create a new account with the information provided by the user  
+      const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+      //If there are any errors during creating the account we inform the user about it
+      promise.catch(e =>
+        this.setState({
+          password1: '',
+          password2: '',
+          error: e.message
+        }));
+      //If no errors where founded the user will be redirected to the registration module
+      promise.then(user => {
+        this.setState({
+          redirectToRegister: true
+        })
+      });
     }
-  });
-  
-    /* TODO */
+    else {
+      this.setState({
+        password1: '',
+        password2: '',
+        error: 'The passwords must be equal.'
+      });
+    }
   }
 
   render() {
-    const { from } = this.props.location.state || { from: { pathname: '../Registration/Registration.js' } }
-        const { redirectToRegister } = this.state;
-        //if the redirectToRegister value is true, then we redirect to the registration page
+    const hidenMenus = document.getElementById('hidenMenus');
+    const defaultMenus = document.getElementById('defaultMenus');
+
+    const { from } = this.props.location.state || { from: { pathname: '/registration' } }
+    const { redirectToRegister } = this.state;
+    //if the redirectToRegister value is true, then we redirect to the registration page
     if (redirectToRegister) {
-        return (
-          <Redirect to={from} />
-        )
-      }
+      hidenMenus.classList.remove('menuHide');
+      defaultMenus.classList.add('menuHide');
+      return (
+        <Redirect to={from} />
+      )
+    }
     const { classes } = this.props;
 
     return (
@@ -145,30 +119,30 @@ class SignUp extends React.Component {
             </Typography>
           </Grid>
           <form onSubmit={this.handleSubmit}>
-          <Grid item xs={12} md={3} lg={3}>
-            <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
-              <InputLabel htmlFor="mail">E-mail</InputLabel>
-              <Input id="mail" name="mail" type="email" value={this.state.mail} onChange={this.handleChange} />
-              <FormHelperText id="required">Required*</FormHelperText>
-            </FormControl>
-            <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input id="password" name="password1" type="Password" value={this.state.password1} onChange={this.handleChange} />
-              <FormHelperText id="required">Required*</FormHelperText>
-            </FormControl>
-            <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
-              <InputLabel htmlFor="passwordConfirm">Confirm Password</InputLabel>
-              <Input id="passwordConfirm" name="password2" type="Password" value={this.state.password2} onChange={this.handleChange} />
-              <FormHelperText id="required">Required*</FormHelperText>
-            </FormControl>
-            <div>
-            <Button variant="contained" color="primary" className={classes.button}><Link to="/log-in">I have an account</Link></Button>
-            <Button variant="contained" type="submit" color="primary" className={classes.button}>
-              Create an Account
+            <Grid item xs={12} md={3} lg={3}>
+              <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
+                <InputLabel htmlFor="mail">E-mail</InputLabel>
+                <Input id="mail" name="mail" type="email" value={this.state.mail} onChange={this.handleChange} />
+                <FormHelperText id="required">Required*</FormHelperText>
+              </FormControl>
+              <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input id="password" name="password1" type="Password" value={this.state.password1} onChange={this.handleChange} />
+                <FormHelperText id="required">Required*</FormHelperText>
+              </FormControl>
+              <FormControl className={(classes.formControl, classes.textField)} aria-describedby="required" aria-required="true">
+                <InputLabel htmlFor="passwordConfirm">Confirm Password</InputLabel>
+                <Input id="passwordConfirm" name="password2" type="Password" value={this.state.password2} onChange={this.handleChange} />
+                <FormHelperText id="required">Required*</FormHelperText>
+              </FormControl>
+              <div>
+                <Button variant="contained" color="primary" className={classes.button}><Link to="/log-in">I have an account</Link></Button>
+                <Button variant="contained" type="submit" color="primary" className={classes.button}>
+                  Create an Account
             </Button>
-            </div>
-          </Grid>
-           <FormHelperText id="error" name="error" value={this.state.error} onChange={this.handleChange}>{this.state.error}</FormHelperText>
+              </div>
+            </Grid>
+            <FormHelperText id="error" name="error" value={this.state.error} onChange={this.handleChange}>{this.state.error}</FormHelperText>
           </form>
         </Grid>
       </div>
