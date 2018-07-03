8dd1a8c87f135d664f5cd765b7ef7b4bd35b8b5f
perlajarillo <perlai.jarillo@gmail.com>
2018-07-03 10:14:01 -0400

    The states are being changed according to the firebase.Promise return. Also, changes in the view are included now in the render().

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -7,35 +7,22 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { Typography } from '@material-ui/core';
 import firebase from '../firebase.js';
-import Header from '../Header/Header';
-// import route Components here
+// importing route Components
 import {
-  BrowserRouter as Router,
-  Route,
   Link,
-  Switch,
   Redirect
 } from 'react-router-dom'
 
-//const auth=firebase.auth();
 const styles = theme => ({
   root: {
     margin: theme.spacing.unit * 6,
-
   },
-  /* container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-  }, */
   formControl: {
     margin: theme.spacing.unit,
   },
   button: {
     margin: theme.spacing.unit,
-    /* fullWidth: 'false', */
   },
   textField: {
     maxWidth: 400,
@@ -57,15 +44,15 @@ class LogIn extends React.Component {
       password1: '',
       error: '',
       redirectToRegister: false
-
     };
-    //If there is a currentUser means the user press logOut in an active session.
-    const hidenMenus=document.getElementById('hidenMenus');
-	const defaultMenus=document.getElementById('defaultMenus');
-    if (firebase.auth().currentUser){
-    	firebase.auth().signOut();
-    	hidenMenus.classList.add('menuHide');
-	    defaultMenus.classList.remove('menuHide');
+
+    /*If there is a currentUser means the user press logOut in an active session.
+    we will close the session and change state.*/
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut();
+      this.setState({
+        redirectToRegister: false
+      });
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -75,65 +62,53 @@ class LogIn extends React.Component {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    console.log('target', target, 'name', name, 'value', value);
     this.setState({
-      [name]: value,
-      [name]: value,
-      [name]: value,
       [name]: value
     });
   };
 
   handleSubmit(e) {
-	const hidenMenus=document.getElementById('hidenMenus');
-	const defaultMenus=document.getElementById('defaultMenus');  	
     e.preventDefault();
-    var email=this.state.mail;
-    var password=this.state.password1;
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-       console.log(error.code);
-       console.log(error.message);
+    const email = this.state.mail;
+    const password = this.state.password1;
+    //Attempting to signIn with email and password and creating a promise to manage the error
+    const promise = firebase.auth().signInWithEmailAndPassword(email, password);
+    /*If there is an error, we will capture it and change the state of error 
+    to inform the user something went wrong*/
+    promise.catch(e =>
+      this.setState({
+        error: e.message
+      }));
+    //Adding an observer for changes to the user's sign-in state. 
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          redirectToRegister: true
+        })
+      }
     });
-    var user = firebase.auth().currentUser;
-    //If the user was autenthicated then we show the propor menus
-    firebase.auth().onAuthStateChanged(user =>{
-    	if (user) {
-	    alert("User made log in: "+user.email+ " and we must redirect to the demographic section");
-    	hidenMenus.classList.remove('menuHide');
-	    defaultMenus.classList.add('menuHide');
-	    this.setState({
-		    mail: '',
-		    password1: '',
-		    error:'',
-		    redirectToRegister:true
- 	 	});
-		}
-		else
-		{
-			this.setState({
-			    mail: '',
-			    password1: '',
-			    error:'Invalid credentials, please try again.',
-			    redirectToRegister:false
-		    });
-		} 
-	});
-	//We also need to change the state of the object
-
-    /* TODO */
   }
 
-
-	render() {
-		const { from } = this.props.location.state || { from: { pathname: '../Registration/Registration.js' } }
-      	const { redirectToRegister } = this.state;
-      	//if the redirectToRegister value is true, then we redirect to the registration page
-		if (redirectToRegister) {
-        return (
-          <Redirect to={from} />
-        )
-      }
+  render() {
+    const hidenMenus = document.getElementById('hidenMenus');
+    const defaultMenus = document.getElementById('defaultMenus');
+    if (hidenMenus) {
+      hidenMenus.classList.add('menuHide');
+      defaultMenus.classList.remove('menuHide');
+    }
     const { classes } = this.props;
+    const { from } = this.props.location.state || { from: { pathname: '/registration' } }
+    const { redirectToRegister } = this.state;
+    /*if the redirectToRegister value is true, then we redirect to the registration page
+    and show the proper menu bar*/
+    if (redirectToRegister) {
+      hidenMenus.classList.remove('menuHide');
+      defaultMenus.classList.add('menuHide');
+      return (
+        <Redirect to={from} />
+      )
+    }
+
     return (
       <div>
         <h1>Log In</h1>
@@ -150,18 +125,18 @@ class LogIn extends React.Component {
               <FormHelperText id="required">Required*</FormHelperText>
             </FormControl>
             <div>
-            <Button variant="contained" color="primary" className={classes.button}><Link to="/sign-up_disclamer">Create an account</Link></Button>
-            <Button variant="contained" type="submit" color="primary" className={classes.button}>
-              Log-in
+              <Button variant="contained" color="primary" className={classes.button}><Link to="/sign-up_disclamer">Create an account</Link></Button>
+              <Button variant="contained" type="submit" color="primary" className={classes.button}>
+                Log-in
             </Button>
             </div>
           </Grid>
           <FormHelperText id="error" name="error" value={this.state.error} onChange={this.handleChange}>{this.state.error}</FormHelperText>
-          </form>
+        </form>
 
       </div>
     );
-}
+  }
 }
 LogIn.propTypes = {
   classes: PropTypes.object.isRequired,
