59ef036f4d7159448f120b38f1950cb8ff0d9db4
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2018-08-13 19:26:28 +0200

    Adding basic drawing functionality to canvas

--- src/components/TrackPain/Canvas.js
+++ src/components/TrackPain/Canvas.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import body from "../../images/Body-Pictures.jpg";
 import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const colors = ["#4caf50", "#ffd95b", "#ff7043", "#c41c00"];
 
 const styles = theme => ({
-  canvasStyles: {
-    margin: "2px solid black"
-  },
   button: {
-    margin: theme.spacing.unit
+    marginRight: theme.spacing.unit
   },
   input: {
     display: "none"
+  },
+  bgImage: {
+    background: "url(" + body + ")",
+    border: "2px solid #ccc"
   }
 });
 
@@ -22,10 +25,9 @@ class Canvas extends Component {
 
     this.isDrawing = false;
     this.isMouseDown = false;
-    this.line = [];
-    (this.activeColor = null), (this.canvas = React.createRef());
-    this.canvasImage = React.createRef();
-    this.drawImage = this.drawImage.bind(this);
+    this.activeColor = null;
+
+    this.canvas = React.createRef();
     this.getCoords = this.getCoords.bind(this);
     this.startDrawing = this.startDrawing.bind(this);
     this.draw = this.draw.bind(this);
@@ -34,19 +36,11 @@ class Canvas extends Component {
     this.setColor = this.setColor.bind(this);
   }
 
-  componentDidMount() {
-    this.drawImage();
-  }
-
-  drawImage = () => {
-    const ctx = this.canvas.current.getContext("2d");
-    const image = new Image();
-    image.src = body;
-    image.onload = function() {
-      ctx.drawImage(image, 0, 0);
-    };
-  };
-
+  /**
+   * getCoords – gets coordinates from where the user has clicked the screen
+   * @param {Object} event
+   * @return {Number} coordinates X and Y
+   */
   getCoords = event => {
     let rect = this.canvas.current.getBoundingClientRect();
     let startX = event.clientX - rect.x;
@@ -58,19 +52,36 @@ class Canvas extends Component {
     };
   };
 
+  /**
+   * startDrawing – checks if the user has clicked the screen and calls the draw
+   * function
+   * @param {Object} event
+   * @return {void}
+   */
   startDrawing = event => {
     this.isMouseDown = true;
     this.isDrawing = true;
     this.draw(event);
   };
 
+  /**
+   * setColor – picks the color from the selected button
+   * @param {Object} event
+   * @return {String} the selected color
+   */
   setColor = event => {
     this.activeColor = event.target.value;
     return this.activeColor;
   };
 
+  /**
+   * draw – create the context for canvas, sets the styles to draw
+   * @param {Object} event
+   * @return {void}
+   */
   draw = event => {
     if (!this.isDrawing) return;
+    if (!this.activeColor) return;
     const { startX, startY } = this.getCoords(event);
     const ctx = this.canvas.current.getContext("2d");
     ctx.strokeStyle = this.activeColor;
@@ -86,17 +97,25 @@ class Canvas extends Component {
     // go to
     ctx.lineTo(nextX, nextY);
     ctx.stroke();
-
-    this.line.push({ startX, startY, nextX, nextY });
   };
 
+  /**
+   * stopDrawing – checks if the mouse is down and stops the draw function
+   * @return {void}
+   */
   stopDrawing = () => {
     this.isDrawing = false;
     this.isMouseDown = false;
   };
 
-  clearCanvas = ctx => {
-    this.line = [];
+  /**
+   * clearCanvas – clears all coordinates in the canvas
+   * @return {void}
+   */
+  clearCanvas = () => {
+    const ctx = this.canvas.current.getContext("2d");
+    this.activeColor = null;
+    ctx.clearRect(0, 0, this.canvas.current.width, this.canvas.current.height);
   };
 
   render() {
@@ -104,33 +123,46 @@ class Canvas extends Component {
     return (
       <div>
         <div>
+          <Typography variant="subheading">
+            Pick a color that best represents the intensity of your pain and
+            draw where you felt it.
+          </Typography>
           {colors.map(color => (
-            <button
+            <Button
               key={color}
               value={color}
+              variant="contained"
+              size="small"
               style={{
-                background: color,
-                width: "32px",
-                height: "32px",
-                marginRight: "16px"
+                background: color
               }}
               onClick={this.setColor}
-            />
+            >
+              {" "}
+            </Button>
           ))}
         </div>
         <canvas
           ref={this.canvas}
-          width={500}
-          height={750}
-          style={{ border: "2px solid #ccc" }}
+          width={300}
+          height={550}
+          className={classes.bgImage}
           onMouseDown={this.startDrawing}
           onMouseMove={this.draw}
           onMouseUp={this.stopDrawing}
           onMouseLeave={this.stopDrawing}
         />
-        <button name="clearCanvas" onClick={this.clearCanvas}>
-          Clear
-        </button>
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            size="medium"
+            className={classes.button}
+            onClick={this.clearCanvas}
+          >
+            Clear
+          </Button>
+        </div>
       </div>
     );
   }
--- src/components/TrackPain/TrackPain.js
+++ src/components/TrackPain/TrackPain.js
@@ -4,10 +4,11 @@ import { withStyles } from "@material-ui/core/styles";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Icon from "@material-ui/core/Icon";
+import Slider from "@material-ui/lab/Slider";
 import DateFnsUtils from "material-ui-pickers/utils/date-fns-utils";
 import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsProvider";
 import DateTimePicker from "material-ui-pickers/DateTimePicker";
-import Icon from "@material-ui/core/Icon";
 import { format } from "date-fns";
 import distanceInWordsStrict from "date-fns/formatDistanceStrict";
 import Canvas from "./Canvas";
@@ -15,13 +16,23 @@ import Canvas from "./Canvas";
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    margin: theme.spacing.unit * 7
+    margin: theme.spacing.unit * 3
   },
   container: {
     display: "flex",
     flexWrap: "wrap",
-    width: 250,
-    marginTop: theme.spacing.unit * 7
+    marginTop: theme.spacing.unit * 3
+  },
+  dpMargin: {
+    marginTop: theme.spacing.unit * 6,
+    marginRight: theme.spacing.unit * 3
+  },
+  sectionMargin: {
+    marginTop: theme.spacing.unit * 6
+  },
+  slider: {
+    maxWidth: 400,
+    margin: theme.spacing.unit * 3
   }
 });
 
@@ -38,90 +49,132 @@ class TrackPain extends Component {
 
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    this.setEventDuration = this.setEventDuration.bind(this);
     this.setPainIntensity = this.setPainIntensity.bind(this);
   }
-  handleStartDateChange = date => {
-    this.setState({ startDate: date });
-  };
-  handleStartDateChange = date => {
-    this.setState({ startDate: date });
-  };
 
-  handleEndDateChange = date => {
-    this.setState({ endDate: date });
-  };
-
-  setPainIntensity(event) {
+  /**
+   * componentDidMount – sets in the state today's date and format it
+   * @returns {void}
+   */
+  componentDidMount = () => {
+    const today = format(new Date(), "dddd MMMM D[,] YYYY h[:]mm a");
     this.setState({
-      painIntensity: event.target.value
+      today: today
+    });
+  }
+
+  /**
+   * handleStartDateChange – the handleStartDateChange sets a start date for a
+   * pain event
+   * @param {Object} the date object
+   * @return {void}
+   */
+  handleStartDateChange = date => {
+    this.setState({ startDate: date });
+  };
+
+  /**
+   * handleEndDateChange – the handleEndDateChange sets a end date for a
+   * pain event
+   * @param {Object} the date object
+   * @return {void}
+   */
+  handleEndDateChange = date => {
+    this.setState({ endDate: date }, () => this.setEventDuration());
+  };
+
+  /**
+   * setEventDuration – setEventDuration calculates the difference between start and end dates
+   * @return {void}
+   */
+  setEventDuration = () => {
+    const { endDate, startDate } = this.state;
+    let eventDuration = distanceInWordsStrict(endDate, startDate, "h");
+    this.setState({
+      eventDuration:
+      eventDuration === "0 seconds"
+          ? "Duration: 00"
+          : "Duration: " + eventDuration
+    });
+  };
+
+  /**
+   * setPainIntensity – Sets in the state a number from 1 to 10
+   * @param {Object} event the event object
+   * @param {Number} value a number between 1 and 10
+   * @return {void}
+   */
+  setPainIntensity(event, value) {
+    this.setState({
+      painIntensity: value
     });
   }
 
   render() {
     const { classes } = this.props;
-    const { startDate, endDate, painIntensity } = this.state;
-    const today = format(new Date(), "dddd MMMM D[,] YYYY h[:]mm a");
-    let eventDuration = distanceInWordsStrict(endDate, startDate, "h");
-    let duration =
-      eventDuration === "0 seconds"
-        ? "Duration: 00"
-        : "Duration: " + eventDuration;
+    const { today, startDate, endDate, painIntensity, eventDuration } = this.state;
 
     return (
       <div className={classes.root}>
-       <Typography variant="title">{today}</Typography>
-        <Grid container>
-          <Grid item lg={4}>
-        <Typography variant="body1">
-          Choose a time frame for your pain
-        </Typography>
+        <Grid container spacing={16}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography variant="headline">{today}</Typography>
           </Grid>
-          <Grid item lg={8}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <DateTimePicker
-            value={startDate}
-            onChange={this.handleStartDateChange}
-            label="Start"
-            minDate={"2000/01/01"}
-            maxDate={new Date()}
-            disableOpenOnEnter
-            disableFuture={true}
-          />
-          <DateTimePicker
-            value={endDate}
-            onChange={this.handleEndDateChange}
-            label="End"
-            minDate={"2000/01/01"}
-            maxDate={new Date()}
-            disableOpenOnEnter
-            disableFuture={true}
-          />
-            </MuiPickersUtilsProvider>
-        <Typography variant="subheading">{duration}</Typography>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div className={classes.sectionMargin}>
+              <Typography variant="title">
+                Choose a time frame for your pain
+              </Typography>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DateTimePicker
+                  value={startDate}
+                  onChange={this.handleStartDateChange}
+                  label="Start"
+                  minDate={"2000/01/01"}
+                  maxDate={new Date()}
+                  disableOpenOnEnter
+                  disableFuture={true}
+                  className={classes.dpMargin}
+                />
+                <DateTimePicker
+                  value={endDate}
+                  onChange={this.handleEndDateChange}
+                  label="End"
+                  minDate={"2000/01/01"}
+                  maxDate={new Date()}
+                  disableOpenOnEnter
+                  disableFuture={true}
+                  className={classes.dpMargin}
+                />
+              </MuiPickersUtilsProvider>
+              <Typography variant="title" className={classes.sectionMargin}>
+                {eventDuration}
+              </Typography>
+            </div>
+            <div className={classes.sectionMargin}>
+              <Typography variant="subheading">
+                How was the pain intensity during that time frame, 0 being no
+                pain at all and 10 the worst pain imaginable
+              </Typography>
+              <div className={classes.slider}>
+                <Typography id="label">Pain Intensity: ({painIntensity})</Typography>
+                <Slider
+                  value={painIntensity}
+                  min={0}
+                  max={10}
+                  step={1}
+                  onChange={this.setPainIntensity}
+                />
+              </div>
+            </div>
           </Grid>
-          <Grid item lg={4}>
-          <Typography variant="subheading">
-            How was the pain intensity during that time frame, being 0 not pain
-            at all and 10 the worst pain imaginable
-          </Typography>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div className={classes.sectionMargin}>
+              <Canvas />
+            </div>
           </Grid>
-          <Grid item lg={8}>
-            <fieldset className={classes.container}>
-          <FormHelperText>Pain Intensity</FormHelperText>
-          <input
-            type="range"
-            name="pain intensity"
-            value={painIntensity}
-            min="0"
-            max="10"
-            onChange={this.setPainIntensity}
-          />
-            <Typography variant="body1" style={{justifyContent: "flex-start"}}>No Pain</Typography>
-            <Typography variant="body1" style={{justifyContent: "flex-end"}}>Worst Pain</Typography>
-            </fieldset>
-            </Grid>
-          </Grid>
-            <Canvas />
+        </Grid>
       </div>
     );
   }
