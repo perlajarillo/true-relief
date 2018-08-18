4c11d2f8f15cfafb6eb2810ca24dad4f8005b4d3
perlajarillo <perlai.jarillo@gmail.com>
2018-08-17 22:54:10 -0400

    Just leaving the Firebase Operations here

--- src/components/FirebaseOperations.js
+++ src/components/FirebaseOperations.js
@@ -1,21 +1,4 @@
 import firebase from './firebase.js';
-import 'firebase/auth';
-
-export function getActiveUser()
-{
-  const getUserStatus = function () {
-    return new Promise(function (resolve, reject) {
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-          resolve(user.uid);
-        } else {
-          reject(Error('No active session was founded'));
-        }
-      });
-    });
-  };
-  return getUserStatus;
-}
 
 export function writeNewPatient(userId,data){
     //We are using the user.uid attribute as the new key, to identify the patient
