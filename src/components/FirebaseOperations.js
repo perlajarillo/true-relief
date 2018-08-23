20c559c6d8468e218530a099059d31a91be8092c
perlajarillo <perlai.jarillo@gmail.com>
2018-08-23 09:58:40 -0400

    Including the writeNewTrackPain that add the track pain for the active user

--- src/components/FirebaseOperations.js
+++ src/components/FirebaseOperations.js
@@ -4,3 +4,11 @@ export function writeNewPatient(userId,data){
     //We are using the user.uid attribute as the new key, to identify the patient
     firebase.database().ref().child("patients").child(userId).set(data);
 }
+
+export function writeNewTrackPain(userId,data){
+  const newTrackPainKey = firebase.database().ref().child("patients")
+    .child(userId).child("trackPain").push().key;
+  const updates = {};
+  updates['/patients/' + userId+ '/trackPain/'+ newTrackPainKey] = data;
+  return firebase.database().ref().update(updates);
+}
