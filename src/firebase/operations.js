3128fb800c06c4edc9e6bf020ec6c9f532f3c7c5
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2018-09-04 08:34:56 +0000

    Adding "ref" to all db declarations

--- src/firebase/operations.js
+++ src/firebase/operations.js
@@ -1,14 +1,14 @@
 import { db } from './firebase.js';
 
-export function writeNewPatient(userId,data){
+export function writeNewPatient(userId,data) {
     //We are using the user.uid attribute as the new key, to identify the patient
-    db.child("patients").child(userId).set(data);
+    db.ref().child("patients").child(userId).set(data);
 }
 
-export function writeNewTrackPain(userId,data){
-  const newTrackPainKey = db.child("patients")
+export function writeNewTrackPain(userId,data) {
+  const newTrackPainKey = db.ref().child("patients")
     .child(userId).child("trackPain").push().key;
   const updates = {};
   updates['/patients/' + userId+ '/trackPain/'+ newTrackPainKey] = data;
-  return db.update(updates);
+  return db.ref().update(updates);
 }
