ba82102c55154462d3ae5761d4e5e26bbea0cf2f
Perla Jarillo <perlai.jarillo@gmail.com>
2018-11-02 11:32:29 -0400

    Firebase has changed
    Firebase has changed in the last PR, we want to keep that code.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,12 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  **YOUR-CONFIG-HERE **
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -11,7 +16,4 @@ firebase.initializeApp(config);
 const db = firebase.database();
 const auth = firebase.auth();
 
-export {
-  auth,
-  db
-}
+export { auth, db };
