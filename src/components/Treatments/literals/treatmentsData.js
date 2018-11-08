2994fe3c111b1a2ec47a6d70c17bf3b66000d85f
perlajarillo <perlai.jarillo@gmail.com>
2018-11-08 16:27:27 -0500

    treatments data first draw

---
+++ src/components/Treatments/literals/treatmentsData.js
@@ -0,0 +1,26 @@
+const treatmentsData = [
+  {
+    name: "Excercise",
+    description: "Exercise can help to decrease the pain intensity",
+    source:
+      "http://annals.org/aim/fullarticle/2603228/noninvasive-treatments-acute-subacute-chronic-low-back-pain-clinical-practice",
+    conditions: ["Chronical lower-back pain"]
+  },
+  {
+    name: "Multidisciplinary Rehabilitation",
+    description:
+      "Consists of physical treatment (e.g., heat, electrotherapeutic modalities, stretching, strengthening, manual therapy) plus a psychological component, a social or work-related component, or both.",
+    source:
+      "http://annals.org/aim/fullarticle/2603228/noninvasive-treatments-acute-subacute-chronic-low-back-pain-clinical-practice",
+    conditions: ["Chronical lower-back pain", "Neck pain", "Cluster headaches"]
+  },
+  {
+    name: "Acupuncture",
+    description:
+      "Technique in which practitioners stimulate specific points on the body—most often by inserting thin needles through the skin.",
+    source:
+      "https://effectivehealthcare.ahrq.gov/topics/nonpharma-treatment-pain/research-2018",
+    conditions: ["Neck pain"]
+  }
+];
+export default treatmentsData;
